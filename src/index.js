546fe16790cbe079be7545bf2b14e2f5c3e79d1b
Ioana Caraman <ioanacaraman3@gmail.com>
2023-07-06 16:51:54 +0300

    Dictionary - JS - commit after codereview

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 function checkWord() {
     let inputValue=document.getElementById('dictionaryInput').value;
-    if (localStorage.getItem(inputValue) === null) {
+    if (localStorage.getItem(inputValue)===null) {
         document.getElementById('dictionaryMessage').innerText="Word not found";
         document.getElementById("saveButton").disabled=false;
-    }else {
+    } else {
         document.getElementById('dictionaryMessage').innerText="Word found";
     }
 }
 
 function addWord() {
     let inputValue = document.getElementById('dictionaryInput').value;
+
     if (localStorage.getItem(inputValue) === null) {
         localStorage.setItem(inputValue, inputValue);
         document.getElementById('dictionaryMessage').innerText="Word successfully added";
-    }else {
+    } else {
         document.getElementById('dictionaryMessage').innerText="Word already exists in dictionary";
     }
 }
